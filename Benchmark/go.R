13ffcb5b7b2a91e8ae2b1180369eed04b146f652
Zhang, Feng <jumphone@163.com>
2019-03-08 10:45:07 -0500

    Update go.R

--- Benchmark/go.R
+++ Benchmark/go.R
@@ -1,1 +1,28 @@
 
+load('TSNE.RData')
+library('Seurat')
+
+
+
+#####Load Zeisel#######
+ori_label=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
+pbmc@meta.data$ori=ori_label[,2]
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    }
+sc_tag=cbind(names(pbmc@ident), as.character(pbmc@meta.data$ori))    
+exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
+D1=exp_sc_mat
+
+###Load DevOlig#######
+EXP=readRDS('GSE75330.RDS')
+D2=as.matrix(EXP@raw.data)
+#############
+
+
+
+
