cf86307773a2bc63fb0aebb5421628850262a06d
Zhang, Feng <jumphone@163.com>
2019-03-08 10:45:15 -0500

    Update go.R

--- Benchmark/go.R
+++ Benchmark/go.R
@@ -1,10 +1,11 @@
 
-load('TSNE.RData')
+
 library('Seurat')
 
 
 
 #####Load Zeisel#######
+load('TSNE.RData')
 ori_label=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
 pbmc@meta.data$ori=ori_label[,2]
 COL=c()
