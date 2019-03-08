3448aa4165896eab158d43813a564a821bd075c0
Zhang, Feng <jumphone@163.com>
2019-03-08 11:00:34 -0500

    Update go.R

--- Benchmark/go.R
+++ Benchmark/go.R
@@ -16,12 +16,23 @@ while(i <=length(pbmc@ident)){
 sc_tag=cbind(names(pbmc@ident), as.character(pbmc@meta.data$ori))    
 exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 D1=exp_sc_mat
-
+dim(D1)
+#19972  3005
 ###Load DevOlig#######
 EXP=readRDS('GSE75330.RDS')
 D2=as.matrix(EXP@raw.data)
+dim(D2)
+#23556  5069
 #############
 
+mybeer <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=2)
 
+pbmc=mybeer$seurat
+
+PCUSE <- which(mybeer$cor>0.7 & mybeer$fdr<0.05)
+# UMAP:
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+#DimPlot(pbmc, reduction.use='umap', group.by='map', pt.size=0.1)
 
 
