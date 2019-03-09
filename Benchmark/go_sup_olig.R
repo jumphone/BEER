80fc6e3b0a098ba3ae19e99a581eff08d3b47f49
Zhang, Feng <jumphone@163.com>
2019-03-09 18:10:28 -0500

    Update go_sup_olig.R

--- Benchmark/go_sup_olig.R
+++ Benchmark/go_sup_olig.R
@@ -6,6 +6,15 @@ MGH93=read.table('MGH93.txt',header=T,row.names=1,sep='\t')
 MGH97=read.table('MGH97.txt',header=T,row.names=1,sep='\t')
 
 
+library(Seurat)
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+
+
+D1 <- MGH36
+D2 <- MGH53
+mybeer <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=2)
+
+
 
 
 
