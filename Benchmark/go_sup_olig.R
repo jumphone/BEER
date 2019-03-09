2fea4b22da616209a478dbf5d403223daea13f24
Zhang, Feng <jumphone@163.com>
2019-03-09 17:32:13 -0500

    Create go_sup_olig.R

---
+++ Benchmark/go_sup_olig.R
@@ -0,0 +1,16 @@
+MGH36=read.table('MGH36.txt',header=T,row.names=1,sep='\t')
+MGH53=read.table('MGH53.txt',header=T,row.names=1,sep='\t')
+MGH54=read.table('MGH54.txt',header=T,row.names=1,sep='\t')
+MGH60=read.table('MGH60.txt',header=T,row.names=1,sep='\t')
+MGH93=read.table('MGH93.txt',header=T,row.names=1,sep='\t')
+MGH97=read.table('MGH97.txt',header=T,row.names=1,sep='\t')
+
+
+
+
+
+
+
+
+
+
