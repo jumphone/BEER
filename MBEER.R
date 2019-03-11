610a4ae18a2248a48882c0e943f3d8058b9395f3
Zhang, Feng <jumphone@163.com>
2019-03-11 14:35:27 -0400

    Update MBEER.R

--- MBEER.R
+++ MBEER.R
@@ -1,10 +1,48 @@
 source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
+
+D1=read.table('MGH36_mat.txt',header=T,row.names=1,sep='\t')
+D2=read.table('MGH53_mat.txt',header=T,row.names=1,sep='\t')
+D3=read.table('MGH54_mat.txt',header=T,row.names=1,sep='\t')
+D4=read.table('MGH60_mat.txt',header=T,row.names=1,sep='\t')
+D5=read.table('MGH93_mat.txt',header=T,row.names=1,sep='\t')
+D6=read.table('MGH97_mat.txt',header=T,row.names=1,sep='\t')
+
+BATCH=c(rep('D1',ncol(D1)),
+      rep('D2',ncol(D2)),
+      rep('D3',ncol(D3)),
+      rep('D4',ncol(D4)),
+      rep('D5',ncol(D5)),
+      rep('D6',ncol(D6)) )
+
+
+D12=.simple_combine(D1,D2)$combine
+D34=.simple_combine(D3,D4)$combine
+D56=.simple_combine(D5,D6)$combine
+D1234=.simple_combine(D12,D34)$combine
+D123456=.simple_combine(D1234,D56)$combine
+
+DATA=D123456
+dim(DATA) #23686  4347
+rm(D1)
+rm(D2)
+rm(D3)
+rm(D4)
+rm(D5)
+rm(D6)
+rm(D12)
+rm(D34)
+rm(D56)
+rm(D1234)
+rm(D123456)
 
 
 
 
 
-BEER <- function(DATA, TAG, CNUM=10, PCNUM=50, VPCOR=0, CPU=4, print_step=10){
+
+
+BEER <- function(DATA, BATCH, CNUM=10, PCNUM=50, VPCOR=0, CPU=4, print_step=10){
   
   
     
