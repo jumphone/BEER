7cba6532c2c41e9668b08bb96b92e362a8331d3e
Zhang, Feng <jumphone@163.com>
2019-06-03 16:23:29 -0400

    Update go_revise.R

--- Benchmark/go_revise.R
+++ Benchmark/go_revise.R
@@ -34,9 +34,11 @@ saveRDS(mybeer5, 'mybeer5.RDS')
 saveRDS(mybeer50, 'mybeer50.RDS')
 saveRDS(mybeer100, 'mybeer100.RDS')
 
+mybeer3010 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=2,PP=30)
 mybeer3050 <- BEER(D1, D2, CNUM=50, PCNUM=50, CPU=2,PP=30)
 mybeer30100 <- BEER(D1, D2, CNUM=100, PCNUM=50, CPU=2,PP=30)
 
+saveRDS(mybeer3010, 'mybeer3010.RDS')
 saveRDS(mybeer3050, 'mybeer3050.RDS')
 saveRDS(mybeer30100, 'mybeer30100.RDS')
 
@@ -47,4 +49,37 @@ LABEL[which(LABEL %in% c('MFOL1_batch2','MFOL2_batch2'))]='Myelin-forming Oligod
 LABEL[which(LABEL %in% c('NFOL1_batch2','NFOL2_batch2'))]='Newly-formed Oligodendrocytes_batch2'
 LABEL[which(LABEL %in% c('COP_batch2'))]='Differentiation-committed oligodendrocyte precursors_batch2'
 
+
+
+mybeer=mybeer5
+
+pbmc <- mybeer$seurat
+pbmc@meta.data$label=LABEL
+PCUSE <- which(mybeer$cor> min(0.7, median(mybeer$cor))  & mybeer$fdr<0.05)
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
+pdf('mybeer5.pdf',width=15,height=12)
+DimPlot(pbmc, reduction.use='umap', group.by='label', pt.size=0.1,do.label=T)
+dev.off()
+
+
+mybeer=mybeer50
+
+pbmc <- mybeer$seurat
+pbmc@meta.data$label=LABEL
+PCUSE <- which(mybeer$cor> min(0.7, median(mybeer$cor))  & mybeer$fdr<0.05)
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
+pdf('mybeer50.pdf',width=15,height=12)
+DimPlot(pbmc, reduction.use='umap', group.by='label', pt.size=0.1,do.label=T)
+dev.off()
+
+
+mybeer=mybeer100
+
+pbmc <- mybeer$seurat
+pbmc@meta.data$label=LABEL
+PCUSE <- which(mybeer$cor> min(0.7, median(mybeer$cor))  & mybeer$fdr<0.05)
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
+pdf('mybeer100.pdf',width=15,height=12)
+DimPlot(pbmc, reduction.use='umap', group.by='label', pt.size=0.1,do.label=T)
+dev.off()
 
