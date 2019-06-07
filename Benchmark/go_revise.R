7cfbeef41fdcd8958a722c79856d6295212abbcb
Zhang, Feng <jumphone@163.com>
2019-06-07 01:20:21 -0400

    Update go_revise.R

--- Benchmark/go_revise.R
+++ Benchmark/go_revise.R
@@ -1,5 +1,3 @@
-
-
 #####Load Zeisel#######
 load('TSNE.RData')
 ori_label=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
@@ -21,6 +19,12 @@ EXP=readRDS('GSE75330.RDS')
 D2=as.matrix(EXP@raw.data)
 dim(D2)
 #23556  5069
+LABEL=c(paste0(as.character(ori_label[,2]),'_batch1'), paste0(EXP@meta.data$label,'_batch2') )
+LABEL[which(LABEL %in% c('MOL1_batch2','MOL2_batch2','MOL3_batch2','MOL4_batch2','MOL5_batch2','MOL6_batch2'))]='Mature Oligodendrocytes_batch2'
+LABEL[which(LABEL %in% c('MFOL1_batch2','MFOL2_batch2'))]='Myelin-forming Oligodendrocytes_batch2'
+LABEL[which(LABEL %in% c('NFOL1_batch2','NFOL2_batch2'))]='Newly-formed Oligodendrocytes_batch2'
+LABEL[which(LABEL %in% c('COP_batch2'))]='Differentiation-committed oligodendrocyte precursors_batch2'
+saveRDS(LABEL,'LABEL.RDS')
 
 ################################################################################################
 library('Seurat')
@@ -28,7 +32,7 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 
 
 #length(x = pbmc@var.genes)
-CPU=1
+CPU=2
 mybeer5 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=CPU,PP=5)
 mybeer50 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=CPU,PP=50)
 mybeer100 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=CPU,PP=100)
@@ -36,30 +40,24 @@ saveRDS(mybeer5, 'mybeer5.RDS')
 saveRDS(mybeer50, 'mybeer50.RDS')
 saveRDS(mybeer100, 'mybeer100.RDS')
 
-mybeer3010 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=2,PP=30)
+#mybeer3010 <- BEER(D1, D2, CNUM=10, PCNUM=50, CPU=2,PP=30)
 mybeer3050 <- BEER(D1, D2, CNUM=50, PCNUM=50, CPU=2,PP=30)
 mybeer30100 <- BEER(D1, D2, CNUM=100, PCNUM=50, CPU=2,PP=30)
 
-saveRDS(mybeer3010, 'mybeer3010.RDS')
+#saveRDS(mybeer3010, 'mybeer3010.RDS')
 saveRDS(mybeer3050, 'mybeer3050.RDS')
 saveRDS(mybeer30100, 'mybeer30100.RDS')
 
-
-LABEL=c(paste0(as.character(ori_label[,2]),'_batch1'), paste0(EXP@meta.data$label,'_batch2') )
-LABEL[which(LABEL %in% c('MOL1_batch2','MOL2_batch2','MOL3_batch2','MOL4_batch2','MOL5_batch2','MOL6_batch2'))]='Mature Oligodendrocytes_batch2'
-LABEL[which(LABEL %in% c('MFOL1_batch2','MFOL2_batch2'))]='Myelin-forming Oligodendrocytes_batch2'
-LABEL[which(LABEL %in% c('NFOL1_batch2','NFOL2_batch2'))]='Newly-formed Oligodendrocytes_batch2'
-LABEL[which(LABEL %in% c('COP_batch2'))]='Differentiation-committed oligodendrocyte precursors_batch2'
 
 
 ############################
 mybeer5=readRDS('mybeer5.RDS')
 mybeer50=readRDS('mybeer50.RDS')
 mybeer100=readRDS('mybeer100.RDS')
-mybeer3010=readRDS('mybeer3010.RDS')
+#mybeer3010=readRDS('mybeer3010.RDS')
 mybeer3050=readRDS('mybeer3050.RDS')
 mybeer30100=readRDS('mybeer30100.RDS')
-
+LABEL=readRDS('LABEL.RDS')
 
 
 
@@ -70,13 +68,14 @@ mybeer30100=readRDS('mybeer30100.RDS')
 
 
 mybeer=mybeer5
-
+plot(mybeer$cor)
 pbmc <- mybeer$seurat
 pbmc@meta.data$label=LABEL
 PCUSE <- which(mybeer$cor> min(0.7, median(mybeer$cor))  & mybeer$fdr<0.05)
 pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims.use = PCUSE, check_duplicates=FALSE)
 pdf('mybeer5.pdf',width=15,height=12)
 DimPlot(pbmc, reduction.use='umap', group.by='label', pt.size=0.1,do.label=T)
+DimPlot(pbmc, reduction.use='umap', group.by='map', pt.size=0.1,do.label=T)
 dev.off()
 
 
