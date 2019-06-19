c7bb0540a3046a457ce26d2f9e3b10ef7fb6d877
Zhang, Feng <jumphone@163.com>
2019-06-19 18:21:39 -0400

    Update try.R

--- LAB/try.R
+++ LAB/try.R
@@ -2,6 +2,109 @@ library(Seurat)
 source('BEER.R')
 
 
+D1 <- read.table(unz("demodata.zip","DATA1_MAT.txt"), sep='\t', row.names=1, header=T)
+D2 <- read.table(unz("demodata.zip","DATA2_MAT.txt"), sep='\t', row.names=1, header=T)
+
+# "D1" & "D2" are UMI matrix (or FPKM, RPKM, TPM, PKM ...; Should not be gene-centric scaled data)
+# Rownames of "D1" & "D2" are gene names
+# Colnames of "D1" & "D2" are cell names 
+
+# There shouldn't be duplicated colnames in "D1" & "D2":
+colnames(D1)=paste0('D1_', colnames(D1))
+colnames(D2)=paste0('D2_', colnames(D2))
+D12=.simple_combine(D1,D2)$combine
+
+BATCH=rep('D2',ncol(D1)+ncol(D2))
+BATCH[c(1:ncol(D1))]='D1'
+
+mybeer=ProBEER(D12,BATCH)
+
+# Check selected PCs
+PCUSE=mybeer$select
+COL=rep('black',length(mybeer$cor))
+COL[PCUSE]='red'
+plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,xlab='Rank Correlation',ylab='Linear Correlation')
+
+# Run UMAP
+pbmc <- mybeer$seurat
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+
+
+
+
+
+
+##########
+load('TSNE.RData')
+ori_label=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
+pbmc@meta.data$ori=ori_label[,2]
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    }
+sc_tag=cbind(names(pbmc@ident), as.character(pbmc@meta.data$ori))    
+exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
+D1=exp_sc_mat
+dim(D1)
+#19972  3005
+###Load DevOlig#######
+EXP=readRDS('GSE75330.RDS')
+D2=as.matrix(EXP@raw.data)
+dim(D2)
+
+
+
+D12=.simple_combine(D1,D2)$combine
+
+BATCH=rep('D2',ncol(D1)+ncol(D2))
+BATCH[c(1:ncol(D1))]='D1'
+
+LABEL=readRDS('LABEL.RDS')
+
+mybeer=ProBEER(D12,BATCH)
+
+
+# Check selected PCs
+PCUSE=mybeer$select
+COL=rep('black',length(mybeer$cor))
+COL[PCUSE]='red'
+plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,xlab='Rank Correlation',ylab='Linear Correlation')
+
+PCUSE=which( (rank(RESULT$cor)>=length(RESULT$cor)/2 | RESULT$cor>0.7 )    & (rank(RESULT$lcor) >=length(RESULT$cor)/2 | RESULT$lcor>0.7) ) 
+    
+COL=rep('black',length(mybeer$cor))
+COL[PCUSE]='red'
+plot(mybeer$cor,mybeer$lcor,pch=16,col=COL,xlab='Rank Correlation',ylab='Linear Correlation',xlim=c(0,1),ylim=c(0,1))
+
+
+# Run UMAP
+pbmc <- mybeer$seurat
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+
+
+PCUSE=1:50
+pbmc <- mybeer$seurat
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+
+
+
+pbmc@meta.data$type=LABEL
+DimPlot(pbmc, reduction.use='umap', group.by='type', pt.size=0.1,label=T)
+
+
+
+
+
+
+
+
+
 D1=readRDS('MGH36.RDS')
 D2=readRDS('MGH53.RDS')
 D3=readRDS('MGH54.RDS')
