c1c399e72b90f8a0b65ea8ae644610fefac48ca1
Zhang, Feng <jumphone@163.com>
2019-06-19 17:46:25 -0400

    Update try.R

--- LAB/try.R
+++ LAB/try.R
@@ -1,4 +1,47 @@
 library(Seurat)
+source('BEER.R')
+
+
+D1=readRDS('MGH36.RDS')
+D2=readRDS('MGH53.RDS')
+D3=readRDS('MGH54.RDS')
+D4=readRDS('MGH60.RDS')
+D5=readRDS('MGH93.RDS')
+D6=readRDS('MGH97.RDS')
+
+BATCH=c(rep('D1',ncol(D1)),
+        rep('D2',ncol(D2)),
+        rep('D3',ncol(D3)),
+        rep('D4',ncol(D4)),
+        rep('D5',ncol(D5)),
+        rep('D6',ncol(D6)) )
+        
+D12=.simple_combine(D1,D2)$combine
+D34=.simple_combine(D3,D4)$combine
+D56=.simple_combine(D5,D6)$combine
+D1234=.simple_combine(D12,D34)$combine
+D123456=.simple_combine(D1234,D56)$combine
+
+DATA=D123456   
+
+rm(D1);rm(D2);rm(D3);rm(D4);rm(D5);rm(D6)
+
+
+mybeer=ProBEER(DATA,BATCH)
+
+PCUSE=mybeer$select
+COL=rep('black',length(mybeer$cor))
+COL[PCUSE]='red'
+plot(mybeer$cor,mybeer$lcor,pch=16,col=COL)
+
+pbmc <- mybeer$seurat
+pbmc <- RunUMAP(object = pbmc, reduction.use='pca',dims = PCUSE, check_duplicates=FALSE)
+DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1)
+
+
+
+
+
 pbmc=readRDS('RAW.RDS')
 source('BEER_NEW.R')
 EXP=as.matrix(pbmc@assays$RNA@counts)
