3b08565005b15fa9f46391b07e1b705cb8213527
Zhang, Feng <jumphone@163.com>
2019-06-20 16:11:44 -0400

    Update BEER.R

--- BEER.R
+++ BEER.R
@@ -194,7 +194,7 @@ library(pcaPP)
     #DR=X
     #RANK=rank(DR,ties.method='random')
       
-    CLUST=kmeans(X,centers=30)
+    CLUST=kmeans(X,centers=GNUM)$cluster
     GROUP=paste0(TAG,'_',as.character(CLUST))
     
     print('Group Number:')
