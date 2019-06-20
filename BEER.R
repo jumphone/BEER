065dfdda6452c62a89b7f25a2903928f3ad3c92f
Zhang, Feng <jumphone@163.com>
2019-06-19 23:32:56 -0400

    Update BEER.R

--- BEER.R
+++ BEER.R
@@ -425,7 +425,9 @@ BEER <- function(DATA, BATCH, MAXBATCH='', CNUM=50, PCNUM=50, GN=2000, CPU=4, MT
     RESULT$lfdr=OUT$lfdr
     
     PCUSE=which( (rank(RESULT$cor)>=length(RESULT$cor)/2 | RESULT$cor>0.7 )    & 
-                (rank(RESULT$lcor) >=length(RESULT$cor)/2 | RESULT$lcor>0.7)    ) 
+                (rank(RESULT$lcor) >=length(RESULT$cor)/2 | RESULT$lcor>0.7)   &
+                RESULT$lc1 >0.05
+               ) 
     
     RESULT$select=PCUSE
     
