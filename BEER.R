91eb23d5891087356b4fff493d90f9aca8135499
Zhang, Feng <jumphone@163.com>
2019-06-21 00:30:13 -0400

    Update BEER.R

--- BEER.R
+++ BEER.R
@@ -618,6 +618,47 @@ ReBEER <- function(mybeer, MAXBATCH='',  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTA
 
 
 
+#########################
+
+BEER.bbknn <- function(mybeer, PCUSE, NB=50, NT=10){
+  
+    NB=NB
+    NT=NT
+    mybeer=mybeer
+    PCUSE=PCUSE
+    
+    pca.all=pbmc@reductions$pca@cell.embeddings
+    pca.use=pbmc@reductions$pca@cell.embeddings[,PCUSE]
+    batch=as.character(pbmc@meta.data$batch)
+
+    library(reticulate)
+    #use_python("C:\Users\cchmc\Anaconda3\python")
+
+    anndata = import("anndata",convert=FALSE)
+    bbknn = import("bbknn", convert=FALSE)
+    sc = import("scanpy.api",convert=FALSE)
+
+    adata = anndata$AnnData(X=pca.all, obs=batch)
+    PCNUM=ncol(pca.use)
+
+    sc$tl$pca(adata, n_comps=as.integer(PCNUM))
+    adata$obsm$X_pca = pca.use
+    #NB=50
+    #NT=10
+
+    print(NB)
+    bbknn$bbknn(adata,batch_key=0,neighbors_within_batch=as.integer(NB),n_pcs=as.integer(PCNUM), n_trees =as.integer(NT))
+    #bbknn$bbknn(adata,batch_key=0, n_pcs=as.integer(PCNUM))
+    
+    sc$tl$umap(adata)
+    umap = py_to_r(adata$obsm$X_umap)
+    rownames(umap)=rownames(pbmc@reductions$umap@cell.embeddings)
+    colnames(umap)=colnames(pbmc@reductions$umap@cell.embeddings)
+  
+    return(umap)
+    }
+
+
 
 
 
