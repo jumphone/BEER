f0af59fd85879e5b513eaa261c96b5a0f3847bcc
Zhang, Feng <jumphone@163.com>
2019-07-23 13:01:32 -0400

    Update BEER.lab.R

--- LAB/BEER.lab.R
+++ LAB/BEER.lab.R
@@ -1,97 +1,22 @@
+
 # Batch EffEct Remover for single-cell data (BEER)
 # Author: Feng Zhang
-# Date: June 18, 2019
+# Date: June 27, 2019
 # For Seurat 3
 #
 #source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 
 
 library(Seurat)
-library(pcaPP)
-library(igraph)
-library(sva)
-library(limma)
+#library(pcaPP)
+#library(igraph)
+#library(sva)
+#library(limma)
 
 ############################################################################################
 ############################################################################################
+CORMETHOD='spearman'
 
-.get_cor  <- function(exp_sc_mat, exp_ref_mat, method='kendall',CPU=4, print_step=10, gene_check=FALSE){
-    method=method
-    CPU=CPU
-    print_step=print_step
-    #method = "pearson", "kendall", "spearman"
-    ##################
-    print('Gene number of exp_sc_mat1:')
-    print(nrow(exp_sc_mat))
-    print('Gene number of exp_sc_mat2:')
-    print(nrow(exp_ref_mat))
-    #################
-    library(parallel)
-    #Step 1. get overlapped genes
-    exp_sc_mat=exp_sc_mat[order(rownames(exp_sc_mat)),]
-    exp_ref_mat=exp_ref_mat[order(rownames(exp_ref_mat)),]
-    gene_sc=rownames(exp_sc_mat)
-    gene_ref=rownames(exp_ref_mat)
-    gene_over= gene_sc[which(gene_sc %in% gene_ref)]
-    exp_sc_mat=exp_sc_mat[which(gene_sc %in% gene_over),]
-    exp_ref_mat=exp_ref_mat[which(gene_ref %in% gene_over),]
-    colname_sc=colnames(exp_sc_mat)
-    colname_ref=colnames(exp_ref_mat)
-    ###############
-    print('Number of overlapped genes:')
-    print(nrow(exp_sc_mat))
-    if(gene_check==TRUE){
-    print('Press RETURN to continue:')
-    scan();}
-    ###################
-    #Step 2. calculate prob
-    SINGLE <- function(i){
-        library('pcaPP')
-        exp_sc = as.array(exp_sc_mat[,i])
-        
-        cor_list=rep(0,length(colname_ref))
-        j=1
-        while(j<=length(colname_ref)){
-            exp_ref = as.array(exp_ref_mat[,j])
-            
-            if(method=='kendall'){this_cor=cor.fk(exp_sc,exp_ref)}
-            else{
-            this_cor=cor(exp_sc,exp_ref, method=method)}
-            
-            cor_list[j]=this_cor
-            j=j+1}
-        ################################
-        if(i%%print_step==1){print(i)}
-        return(cor_list)
-        }
-    #######################################
-    cl= makeCluster(CPU,outfile='')
-    RUN = parLapply(cl=cl,1:length(exp_sc_mat[1,]), SINGLE)
-    stopCluster(cl)
-    #RUN = mclapply(1:length(colname_sc), SINGLE, mc.cores=CPU)
-    COR = c()
-    for(cor_list in RUN){
-        COR=cbind(COR, cor_list)}
-    #######################################
-    rownames(COR)=colname_ref
-    colnames(COR)=colname_sc
-    return(COR)
-    }
-
-
-.get_tag_max <- function(COR){
-    RN=rownames(COR)
-    CN=colnames(COR)
-    TAG=cbind(CN,rep('NA',length(CN)))
-    i=1
-    while(i<=length(CN)){
-        this_rn_index=which(COR[,i] == max(COR[,i]))[1]
-        TAG[i,2]=RN[this_rn_index]
-        i=i+1
-        }
-    colnames(TAG)=c('cell_id','tag')
-    return(TAG)
-    }
 
 .simple_combine <- function(exp_sc_mat1, exp_sc_mat2){    
     exp_sc_mat=exp_sc_mat1
@@ -126,7 +51,8 @@ library(limma)
         if(length(this_col)>= min_cell){
             outnames=c(outnames,one)
             if(length(this_col) >1){
-                this_new_ref=apply(exp_sc_mat[,this_col],1,sum)
+                #this_new_ref=apply(exp_sc_mat[,this_col],1,sum)
+                this_new_ref=apply(exp_sc_mat[,this_col],1,mean)
                 }
                 else{this_new_ref = exp_sc_mat[,this_col]}
             NewRef=cbind(NewRef,this_new_ref)
@@ -149,52 +75,6 @@ library(limma)
 
 
 
-.data2one <- function(DATA, GENE, CPU=4, PCNUM=50, SEED=123,  PP=30){
-    
-    if(PP>5 & ncol(DATA)<=300 & ncol(DATA)>100){PP=5;print('The cell number is too small! The perplexity is changed to 5 !')} 
-    if(PP>3 & ncol(DATA)<=100){PP=3;print('The cell number is too small! The perplexity is changed to 3 !')}
-    
-    
-    PCUSE=1:PCNUM
-    print('Start')
-    library(Seurat)
-    print('Step1.Create Seurat Object...')
-    DATA =CreateSeuratObject(counts = DATA, min.cells = 0, min.features = 0, project = "ALL")   
-    print('Step2.Normalize Data...')
-    DATA <- NormalizeData(object = DATA, normalization.method = "LogNormalize", scale.factor = 10000)
-    print('Step3.Scale Data...')
-    DATA <- ScaleData(object = DATA, features = GENE, vars.to.regress = c("nCount_RNA"), num.cores=CPU, do.par=TRUE)
-    print('Step4.PCA...')
-    DATA <- RunPCA(object = DATA, seed.use=SEED, npcs=PCNUM, features = GENE, ndims.print=1,nfeatures.print=1)
-    print('Step5.One-dimention...')
-    DATA <- RunTSNE(object = DATA, seed.use=SEED, dims.use = PCUSE, do.fast=TRUE,dim.embed = 1,  perplexity= PP)
-    DR=DATA@reductions$tsne@cell.embeddings
-    print('Finished!!!')
-    return(DR)
-    }
-
-.getGroupOld <- function(X,TAG,CNUM=10){
-    DR=X
-    RANK=rank(DR,ties.method='random')
-    CUTOFF=CNUM 
-    GROUP=rep('NA',length(RANK))
-    i=1
-    j=1
-    while(i<=length(RANK)){
-        GROUP[which(RANK==i)]=paste0(TAG,'_',as.character(j))
-        #if(i%%CUTOFF==1){j=j+1;print(j)}
-        ########################
-        if(CUTOFF!=1 & i%%CUTOFF==1){j=j+1}
-        ########################
-        if(CUTOFF==1){j=j+1}  
-        ########################
-        i=i+1}
-    print('Group Number:')
-    print(j-1)
-    return(GROUP)
-}
-
-
 .getGroup <- function(X,TAG,GNUM){
     
     print('Get group for:')
@@ -213,119 +93,18 @@ library(limma)
 
 
 
-.getValidpair <- function(DATA1, GROUP1, DATA2, GROUP2, CPU=4, method='kendall', print_step=10){
-    #source('https://raw.githubusercontent.com/jumphone/scRef/master/scRef.R')
-    print_step=print_step
-    method=method
-    CPU=CPU
-    print('Start')
-    print('Step1.Generate Reference...')
-    REF1=.generate_ref(DATA1, cbind(GROUP1, GROUP1), min_cell=1) 
-    REF2=.generate_ref(DATA2, cbind(GROUP2, GROUP2), min_cell=1) 
-    print('Step2.Calculate Correlation Coefficient...')
-    out = .get_cor( REF1, REF2, method=method,CPU=CPU, print_step=print_step)
-    print('Step3.Analyze Result...')
-    tag1=.get_tag_max(out)
-    tag2=.get_tag_max(t(out))
-    V=c()
-    i=1
-    while(i<=nrow(tag1)){
-        t1=tag1[i,1]
-        t2=tag1[i,2]
-        if(tag2[which(tag2[,1]==t2),2]==t1){V=c(V,i)}           
-        i=i+1}
-    VP=tag1[V,]
-    ##############################
-    if(length(V)<=1){return(message("If BEER crashed, please try a different GNUM to get valid pair."))}
-    ##############################
-    C=c()
-    t=1
-    while(t<=nrow(VP)){
-        this_c=out[which(rownames(out)==VP[t,2]),which(colnames(out)==VP[t,1])]
-        C=c(C,this_c)
-        t=t+1}
-    #if(do.plot==TRUE){plot(C)}
-    #VP=VP[which(C>=CUTOFF),]  
-    print('Finished!!!')
-    OUT=list()
-    OUT$vp=VP
-    OUT$cor=C
-    return(OUT)
-    }
 
-
-
-
-.getVPnet<- function(pbmc, ROUND){
-    library(igraph)
-    pbmc=pbmc
-    ROUND=ROUND
-
-    ################
-    REF=.generate_ref(pbmc@assays$RNA@data, cbind(pbmc@meta.data$group,pbmc@meta.data$group),min_cell=1)
-    VREF=REF
-    CVREF=cor(VREF,method='spearman')
-    #ROUND=3
-    VP=c()
-    I=1    
-    while(I<=ROUND){
-        
-        p1=c()
-        p2=c()
-        score=c()
-        i=1
-        while(i<=nrow(CVREF)){
-            this_p1=rownames(CVREF)[i]
-            j=i+1
-            while(j<=ncol(CVREF)){
-                this_p2=colnames(CVREF)[j]  
-                p1=c(p1,this_p1)
-                p2=c(p2,this_p2)
-            
-                vp_index=which(VP[1,]==this_p1 & VP[2,]==this_p2) 
-                if(length(vp_index) >0){
-                    this_score=999999 }else{
-                    this_score=1-CVREF[i,j]}         
-                score=c(score,this_score)
-                j=j+1}        
-            i=i+1}
-
-
-        NET = cbind(p1,p2) 
-        g <- make_graph(t(NET),directed = FALSE)
-        MST=mst(g, weights = score, algorithm = NULL)
-        E_MST=as_edgelist(MST, names = TRUE)
-
-        i=1
-        while(i<=nrow(E_MST)){
-            t1=unlist(strsplit(E_MST[i,1],'_'))[1]
-            t2=unlist(strsplit(E_MST[i,2],'_'))[1]
-            if(t1!=t2){VP=cbind(VP,E_MST[i,])}
-                i=i+1}
-
-        print(I)
-        I=I+1
-        }       
-        
-        
-    VP=t(VP)
-    #######################
-    return(VP)
-    }
-
-
-
-
+####################
 
 .getVPall<- function(pbmc, ROUND){
     
     pbmc=pbmc
     ROUND=ROUND
-
+    print('Finding MN pairs...')
     ################
     REF=.generate_ref(pbmc@assays$RNA@data, cbind(pbmc@meta.data$group,pbmc@meta.data$group),min_cell=1)
     VREF=REF
-    CVREF=cor(VREF,method='spearman')
+    CVREF=cor(VREF,method=CORMETHOD)
     orig.CVREF=CVREF
     #ROUND=3
 
@@ -346,7 +125,7 @@ library(limma)
             while(j<=ncol(this_cor_mat)){
                 this_p2=colnames(this_cor_mat)[j]  
                 this_cor=this_cor_mat[i,j]
-                if(this_cor==max(this_cor_mat[i,]) & this_cor==max(this_cor_mat[,j])){                 
+                if( this_cor!= -99999  & this_cor==max(this_cor_mat[i,]) & this_cor==max(this_cor_mat[,j])){                 
                     VP=cbind(VP,c(this_p1,this_p2))
                     }                
                 j=j+1}        
@@ -354,6 +133,8 @@ library(limma)
         return(VP)
     
         }
+    
+    
     
     VP=c()
     I=1    
@@ -364,17 +145,21 @@ library(limma)
             while(i<=ncol(VP)){
                 p1=VP[1,i]
                 p2=VP[2,i]
-                CVREF[which(rownames(CVREF)==p1), which(colnames(CVREF)==p2)]=-99999
-                CVREF[which(rownames(CVREF)==p2), which(colnames(CVREF)==p1)]=-99999    
-                #b1=.get_batch(p1)
-                #b2=.get_batch(p2)
-                #b1i=which(group_batch==b1)
-                #b2i=which(group_batch==b2)  
-                #CVREF[b1i,b2i][which(rownames(CVREF[b1i,b2i])==p1),]= -99999
-                #CVREF[b1i,b2i][,which(colnames(CVREF[b1i,b2i])==p2)]=   -99999                 
+                b1=.get_batch(p1)
+                b2=.get_batch(p2)
+                b1i=which(group_batch==b1)
+                b2i=which(group_batch==b2)
+                #CVREF[which(rownames(CVREF)==p1), which(colnames(CVREF)==p2)]=-99999
+                #CVREF[which(rownames(CVREF)==p2), which(colnames(CVREF)==p1)]=-99999    
+                CVREF[which(rownames(CVREF)==p1),b2i]=-99999
+                CVREF[which(rownames(CVREF)==p2),b1i]=-99999
+                CVREF[b2i,which(rownames(CVREF)==p1)]=-99999
+                CVREF[b1i,which(rownames(CVREF)==p2)]=-99999            
                 i=i+1}
             }
     
+        
+        
         i=1
         while(i<length(UBATCH)){
             j=i+1
@@ -386,19 +171,31 @@ library(limma)
                 this_cor_mat=CVREF[b1i,b2i]
                 this_vp=.getMN(this_cor_mat)    
                 VP=cbind(VP,this_vp)
-            #print(dim(this_cor_mat))
-            #print(b1)
-            #print(b2)
+                
+                ########################
+                                
+                ########################    
+
+                
                 j=j+1}
             i=i+1
             }
+        
+        ########################
+        
+        ######################## 
+        print('ROUND:')
         print(I)
         I=I+1
         }       
         
-        
+    
     VP=t(VP)
+    VP=unique(VP)
     #######################
+    
+    print('Number of MN pairs:')
+    print(nrow(VP))
     return(VP)
     }
 
@@ -436,7 +233,7 @@ library(limma)
                        
             i=i+1}
         
-        this_test=cor.test(lst1_quantile, lst2_quantile, method='kendall')        
+        this_test=cor.test(lst1_quantile, lst2_quantile, method=CORMETHOD)        
         this_cor=this_test$estimate
         this_pv=this_test$p.value
 
@@ -473,7 +270,7 @@ library(limma)
    }
 
 
-BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", REGBATCH=FALSE, print_step=10, SEED=123, N=2, ROUND=1){
+BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", REGBATCH=FALSE, print_step=10, SEED=123, N=2, ROUND=1, RMG=NULL){
 
     set.seed( SEED)
     RESULT=list()
@@ -483,6 +280,11 @@ BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", 
     print(Sys.time())
     DATA=DATA
     BATCH=BATCH
+    RMG=RMG
+    
+    require(stringi)
+    BATCH=stri_replace_all(BATCH, '.',fixed='_')
+      
     GNUM=GNUM
     PCNUM=PCNUM
     MTTAG=MTTAG
@@ -520,6 +322,15 @@ BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", 
     pbmc@meta.data$batch=BATCH
     VariableFeatures(object = pbmc)=VARG
 
+    ########
+    if(!is.null(RMG)){
+        print('Total removed gene number is:')
+        print(length(RMG))
+        VariableFeatures(object = pbmc)=VariableFeatures(object = pbmc)[which(! VariableFeatures(object = pbmc) %in% RMG)]
+        print('Total used gene number is:')
+        print(length(VariableFeatures(object = pbmc)))
+        }
+    ##########   
 
     pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
     pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = MTTAG)
@@ -548,7 +359,6 @@ BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", 
 
     pbmc@meta.data$group=GROUP
     
-    
     ##########
     #VP=.getVPnet(pbmc, ROUND)
     VP=.getVPall(pbmc, ROUND)
@@ -561,7 +371,6 @@ BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", 
     MAP[which(GROUP %in% VP[,2])]='V2'
     pbmc@meta.data$map=MAP
     
-
     OUT=.evaluateProBEER(DR, GROUP, VP)
 
     RESULT=list()
@@ -576,6 +385,19 @@ BEER <- function(DATA, BATCH,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", 
     RESULT$lc1=OUT$lc1
     RESULT$lc2=OUT$lc2
     RESULT$lfdr=OUT$lfdr
+    
+    ################
+    RESULT$ROUND=ROUND
+    RESULT$REGBATCH=REGBATCH
+    RESULT$GNUM=GNUM
+    RESULT$GN=GN
+    RESULT$PCNUM=PCNUM
+    RESULT$MTTAG=MTTAG
+    RESULT$SEED=SEED
+    RESULT$N=N
+    RESULT$APP='BEER'   
+    ###############
+    
     
     PCUSE=which( (rank(RESULT$cor)>=length(RESULT$cor)/2 | RESULT$cor>0.7 )    & 
                 (rank(RESULT$lcor) >=length(RESULT$cor)/2 | RESULT$lcor>0.7)   #&
@@ -600,10 +422,9 @@ MBEER=BEER
     }
 
 
-.selectUSE <-function(RESULT, CUTR=0.7, CUTL=0.7, RR=0.5, RL=0.5, CC=0.05){
+.selectUSE <-function(RESULT, CUTR=0.7, CUTL=0.7, RR=0.5, RL=0.5){
     PCUSE=which( (rank(RESULT$cor)>=length(RESULT$cor)*RR | RESULT$cor>CUTR )    & 
-                (rank(RESULT$lcor) >=length(RESULT$cor)*RR | RESULT$lcor>CUTL)   &
-                 p.adjust(RESULT$lc1,method='fdr') > CC
+                (rank(RESULT$lcor) >=length(RESULT$cor)*RR | RESULT$lcor>CUTL)  
                ) 
     return(PCUSE)
     }
@@ -612,7 +433,7 @@ MBEER=BEER
 
 
 
-ReBEER <- function(mybeer,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", print_step=10, SEED=123, N=2, ROUND=1){
+ReBEER <- function(mybeer,  GNUM=30, PCNUM=50,  CPU=4, MTTAG="^MT-", print_step=10, SEED=123, N=2, ROUND=1, RMG=NULL){
 
     set.seed( SEED)
     RESULT=list()
@@ -624,10 +445,11 @@ ReBEER <- function(mybeer,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", pri
     BATCH=mybeer$seurat@meta.data$batch
     GNUM=GNUM
     PCNUM=PCNUM
+    RMG=RMG
     MTTAG=MTTAG
     #MAXBATCH=MAXBATCH
     UBATCH=unique(BATCH)
-    GN=GN
+    
     ROUND=ROUND
     N=N
     print_step=print_step
@@ -636,11 +458,19 @@ ReBEER <- function(mybeer,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", pri
     
     print('Group number (GNUM) is:')
     print(GNUM)
-    print('Total varible gene number (GN) is:')
+    print('Total varible gene number is:')
     print(length(VariableFeatures(object = pbmc)))
     print('ROUND is:')
     print(ROUND)
-    
+    ########
+    if(!is.null(RMG)){
+        print('Total removed gene number is:')
+        print(length(RMG))
+        VariableFeatures(object = pbmc)=VariableFeatures(object = pbmc)[which(! VariableFeatures(object = pbmc) %in% RMG)]
+        print('Total used gene number is:')
+        print(length(VariableFeatures(object = pbmc)))
+        }
+    ##########
     VARG=VariableFeatures(object = pbmc)
     pbmc <- RunPCA(object = pbmc, seed.use=SEED, npcs=PCNUM, features = VariableFeatures(object = pbmc), ndims.print=1,nfeatures.print=1)
     
@@ -692,7 +522,15 @@ ReBEER <- function(mybeer,  GNUM=30, PCNUM=50, GN=2000, CPU=4, MTTAG="^MT-", pri
     RESULT$lc2=OUT$lc2
     RESULT$lfdr=OUT$lfdr
     
-    #########
+    ################
+    RESULT$ROUND=ROUND
+    RESULT$GNUM=GNUM
+    RESULT$PCNUM=PCNUM
+    RESULT$MTTAG=MTTAG
+    RESULT$SEED=SEED
+    RESULT$N=N
+    RESULT$APP='ReBEER'   
+    ###############
 
     #########
        
@@ -783,5 +621,37 @@ BEER.bbknn <- function(pbmc, PCUSE, NB=3, NT=10){
     }
 
 
+#######2019.07.17
 
 
+
+.readTable <- function(PATH, SEP='\t', UP=FALSE){
+    SEP=SEP
+    PATH=PATH
+    UP=UP
+    DATA=read.table(file=PATH,sep=SEP,header=TRUE,row.names=NULL)
+    DATA=apply(DATA,2,as.character)
+    ###########
+    if(UP==TRUE){DATA[,1]=toupper(DATA[,1])}
+    ###########
+    TAB=table(as.character(DATA[,1]))
+    UNIQ=names(TAB)[which(TAB==1)]
+    DATA=DATA[which(DATA[,1] %in% UNIQ),]
+    RN=DATA[,1]
+    DATA=DATA[,c(2:ncol(DATA))]
+    DATA=apply(DATA,2,as.numeric)
+    rownames(DATA)=RN
+    return(DATA)
+    }
+
+.writeTable <- function(DATA, PATH, SEP='\t',TL='OUTPUT'){
+    DATA=DATA
+    PATH=PATH
+    SEP=SEP
+    TL=TL
+    OUT=cbind(rownames(DATA),DATA)
+    colnames(OUT)[1]=TL
+    write.table(OUT, file=PATH,sep=SEP,row.names=FALSE,col.names=TRUE,quote=FALSE)
+    }
+
+###########
