caf340ecf382291f3e278129c6c8fd38b76cc393
Zhang, Feng <jumphone@163.com>
2019-11-01 11:46:33 -0400

    Update BEER.R

--- BEER.R
+++ BEER.R
@@ -17,10 +17,49 @@ library(limma)
 ############################################################################################
 CORMETHOD='spearman'
 
+####################################
 
-.simple_combine <- function(exp_sc_mat1, exp_sc_mat2){    
+#.simple_combine <- function(exp_sc_mat1, exp_sc_mat2){    
+#    exp_sc_mat=exp_sc_mat1
+#    exp_ref_mat=exp_sc_mat2
+#    exp_sc_mat=exp_sc_mat[order(rownames(exp_sc_mat)),]
+#    exp_ref_mat=exp_ref_mat[order(rownames(exp_ref_mat)),]
+#    gene_sc=rownames(exp_sc_mat)
+#    gene_ref=rownames(exp_ref_mat)
+#    gene_over= gene_sc[which(gene_sc %in% gene_ref)]
+#    exp_sc_mat=exp_sc_mat[which(gene_sc %in% gene_over),]
+#    exp_ref_mat=exp_ref_mat[which(gene_ref %in% gene_over),]
+#    colname_sc=colnames(exp_sc_mat)
+#    colname_ref=colnames(exp_ref_mat)
+#    OUT=list()
+#    OUT$exp_sc_mat1=exp_sc_mat
+#    OUT$exp_sc_mat2=exp_ref_mat
+#    OUT$combine=cbind(exp_sc_mat,exp_ref_mat)
+#    return(OUT)
+#    }
+######################
+# 2019.11.01
+
+.simple_combine <- function(exp_sc_mat1, exp_sc_mat2, FILL=FALSE){    
+    FILL=FILL
     exp_sc_mat=exp_sc_mat1
     exp_ref_mat=exp_sc_mat2
+    ##############################################
+    if(FILL==TRUE){ 
+        gene1=rownames(exp_sc_mat)
+        gene2=rownames(exp_ref_mat)
+        gene12=gene2[which(!gene2 %in% gene1)]
+        gene21=gene1[which(!gene1 %in% gene2)]
+        exp_sc_mat_add=matrix(0,ncol=ncol(exp_sc_mat),nrow=length(gene12))
+        rownames(exp_sc_mat_add)=gene12
+        colnames(exp_sc_mat_add)=colnames(exp_sc_mat)
+        exp_ref_mat_add=matrix(0,ncol=ncol(exp_ref_mat),nrow=length(gene21))
+        rownames(exp_ref_mat_add)=gene21
+        colnames(exp_ref_mat_add)=colnames(exp_ref_mat)
+        exp_sc_mat=rbind(exp_sc_mat, exp_sc_mat_add)
+        exp_ref_mat=rbind(exp_ref_mat, exp_ref_mat_add)
+    }
+    ############################################ 
     exp_sc_mat=exp_sc_mat[order(rownames(exp_sc_mat)),]
     exp_ref_mat=exp_ref_mat[order(rownames(exp_ref_mat)),]
     gene_sc=rownames(exp_sc_mat)
@@ -36,7 +75,11 @@ CORMETHOD='spearman'
     OUT$combine=cbind(exp_sc_mat,exp_ref_mat)
     return(OUT)
     }
-    
+
+######################
+
+
+
 
 
 .generate_ref <- function(exp_sc_mat, TAG, min_cell=1, refnames=FALSE){
