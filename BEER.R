14acc929b969da420c64d1691ac250ab082e6129
Zhang, Feng <15110700005@fudan.edu.cn>
2021-03-09 13:22:11 +0800

    Update BEER.R

--- BEER.R
+++ BEER.R
@@ -752,7 +752,7 @@ BEER.bbknn <- function(pbmc, PCUSE, NB=3, NT=10, DM=2){
     
     anndata = import("anndata",convert=FALSE) 
     bbknn = import("bbknn", convert=FALSE)
-    sc = import("scanpy.api",convert=FALSE) #scanpy==1.3.5
+    sc = import("scanpy.api",convert=FALSE) #scanpy==1.4
     #sc = import("scanpy",convert=FALSE) #scanpy
     
     adata = anndata$AnnData(X=pca.all, obs=batch)
